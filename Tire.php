7f2448908bc2922a1ed899d6132e0fe673a00182
railijs <railijsgrieznis@gmail.com>
2023-11-29 09:19:05 +0200

    raah

---
+++ Tire.php
@@ -0,0 +1,15 @@
+<?php
+
+class Tire {
+    
+
+    public function __construct(
+        public $size,
+         public $type,
+          private $quality) 
+          {}
+
+    public function __destruct() {
+                   echo $this->type . " boom!" . "<br>";
+               }
+}
--- matiss.php
+++ matiss.php
@@ -1,6 +1,7 @@
 <?php
 include "Bird.php";
 include "Car.php";
+include "Tire.php";
 
 // $bird1 = new Bird("Matiss", 12);
 // echo $bird1->name. "<br>";
@@ -9,8 +10,12 @@ include "Car.php";
 // $bird2 = new Bird("Jekabs", 2);
 // $bird2->fly();
 
-$car1 = new Car("Yamaha", 200);
-$car1->drive();
+// $car1 = new Car("Yamaha", 200);
+// $car1->drive();
+
+$tire1 = new Tire(19.4, "summer", "good");
+echo $tire1->size. "<br>"; 
+echo $tire1->type. "<br>";
 
 
 ?>
